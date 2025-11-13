cbcb7e966ba699a2c1f781450d39702e962a5c11
Kapilansh Patil <kapilansh.patil.etrx@ghrce.raisoni.net>
2025-11-13 22:32:27 +0530

    Added GET and DELETE endpoints for content management with authorization checks

--- src/app/api/v1/content/route.ts
+++ src/app/api/v1/content/route.ts
@@ -27,3 +27,45 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json({ message: "Content created "}, { status: 200 })
 }
+
+export async function GET(req: NextRequest) {
+
+    const token = req.headers.get("authorization")?.split(" ")[1];
+    const decoded = verifyToken(token);
+    if(!decoded) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    const content = await prisma.content.findMany({
+        where: { userId: decoded.userId },
+        orderBy: { createdAt: "desc" }
+    })
+
+    return NextResponse.json({ content }, { status: 200 })
+}
+
+export async function DELETE(req: NextRequest) {
+
+    const token = req.headers.get("Authorization")?.split(" ")[1];
+    const decoded = verifyToken(token);
+    if(!decoded) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    const body = await req.json();
+    const { contentId } = body;
+
+    if(!contentId) {
+        return NextResponse.json({ error: "ContentId is required" }), { status: 411 };
+    }
+
+    const content = await prisma.content.findUnique({
+        where: { id: contentId }
+    })
+
+    if(!content || content.userId !== decoded.userId) {
+        return NextResponse.json({ error: "Not allowed to delete this item" }, { status: 403 });
+    }
+
+    await prisma.content.delete({
+        where: { id: contentId }
+    })
+
+    return NextResponse.json({ message: "Content deleted" }, { status: 200 });
+}
