a5eed6ea8cf5daae7b76d3c9f785877e46cc28e1
Kapilansh Patil <kapilansh.patil.etrx@ghrce.raisoni.net>
2025-11-23 13:51:06 +0530

    Added content creation modal and integrate with dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,14 @@
 
 import { useEffect, useState } from "react"
 import { ContentCard } from "../brain/[shareId]/ContentCard";
+import { AddContentModal } from "@/components/AddContentModal";
 
 export default function DashboardPage() {
 
 
     const [content, setContent] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [open, setOpen] = useState(false);
 
 
     useEffect(() => {
@@ -47,6 +49,12 @@ export default function DashboardPage() {
         <div className="min-h-screen bg-black text-white px-4 py-10">
             <h1 className="text-4xl font-bold mb-6 flex items-center justify-center">Your MindDock</h1>
 
+            <button onClick={() => setOpen(true)}
+                className="bg-amber-200 hover:bg-amber-300 px-3 py-3 rounded-lg font-medium text-black mb-4"
+            >
+                + Add Content
+            </button>
+
             <div className="max-w-3xl mx-auto space-y-6">
                 { content.length === 0 ? (
                     <p className="text-gray-400">No content yet. Add something!</p>
@@ -56,6 +64,37 @@ export default function DashboardPage() {
                     ))
                 )}
             </div>
+
+            <AddContentModal 
+                open={open}
+                setOpen={setOpen}
+                onCreate = { async (newContent) => {
+
+                    const token = localStorage.getItem("token")
+
+                    const res = await fetch("api/v1/content", {
+                        method: "POST",
+                        headers: {
+                            "Content-type": "application/json",
+                            "Authorization": `Bearer ${token}`
+                        },
+                        body: JSON.stringify(newContent)
+                    })
+
+                    if(res.ok){
+                        setOpen(false)
+
+                        const updated = await fetch("/api/v1/content", {
+                            headers: { Authorization: `Bearer ${token}`},
+                        }).then((r) => r.json());
+
+                        setContent(updated.content)
+                    }
+
+
+                    console.log("created")
+                }}
+            />
         </div>
     )
 }
---
+++ src/components/AddContentModal.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { useState } from "react";
+
+export function AddContentModal ({ open, setOpen, onCreate }: any) {
+
+
+    const [type, setType] = useState("youtube");
+    const [link, setLink] = useState("");
+    const [title, setTitle] = useState("");
+    const [tags, setTags] = useState("");
+
+    if(!open) return null;
+
+    return (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
+            <div className="bg-neutral-900 p-6 rounded-lg w-full max-w-md shadow-xl">
+                
+                <h2 className="text-2xl font-semibold mb-4">Add new Content</h2>
+
+                <label className="text-sm text-gray-300">Type</label>
+                <select
+                    value={type}
+                    onChange={(e) => setType(e.target.value)}
+                    className="w-full mt-1 mb-3 p-2 rounded bg-neutral-800 outline-none"
+                >
+                    <option value="youtube">Youtube</option>
+                    <option value="link">Link</option>
+                    <option value="tweet">Tweet</option>
+                    <option value="text">Text</option>
+                </select>
+
+                {/* Title */}
+                <label>Title</label>
+                <input 
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className="w-full mt-1 mb-3 p-2 bg-neutral-800 outline-none rounded"
+                    placeholder="e.g Next.js tutorial"
+                />
+
+                {/* Link */}
+                <label>Link</label>
+                <input 
+                    type="text"
+                    value={link}
+                    onChange={(e) => setLink(e.target.value)}
+                    className="w-full mt-1 mb-3 p-2 rounded bg-neutral-800 outline-none"
+                    placeholder="https://..."
+                />
+
+                {/* Tags */}
+                <label>Tags</label>
+                <input 
+                    type="text"
+                    value={tags}
+                    onChange={(e) => setTags(e.target.value)}
+                    className="w-full mt-1 mb-4 p-2 rounded bg-neutral-800 outline-none"
+                    placeholder="webdev, learning" 
+                />
+
+                <div className="flex justify-end gap-4 mt-6">
+                    <button
+                        onClick={() => setOpen(false)}
+                        className="px-4 py-2 bg-neutral-700 rounded-md"
+                    >
+                        Cancel
+                    </button>
+
+                    <button
+                        className="px-4 py-2 bg-red-400 rounded-md"
+                        onClick={() => 
+                            onCreate({
+                                type,
+                                title,
+                                link,
+                                tags: tags.split(",").map((t) => t.trim())
+                            })
+                        }
+                    >
+                        Add
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
