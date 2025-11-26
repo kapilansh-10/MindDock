b6e0660c5d7786c310f9c7bfa8d1ecd4851340f6
Kapilansh Patil <kapilansh.patil.etrx@ghrce.raisoni.net>
2025-11-26 19:30:50 +0530

    chore: update dependencies and refactor Prisma configuration
    - Updated @prisma/adapter-neon to version 7.0.1 and @prisma/client to version 6.7.0.
    - Added @neondatabase/serverless as a new dependency.
    - Removed prisma.config.ts file and moved datasource configuration to schema.prisma.
    - Refactored prisma.ts to simplify PrismaClient initialization and removed unused imports.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,30 +1,15 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// const globalForPrisma = global as unknown as { prisma: PrismaClient}
-
-
-// export const prisma =
-//     globalForPrisma.prisma ||
-//     new PrismaClient({
-//         log: ["query", "error", "warn"],
-//     });
-
-// if(process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
-
-
-import { PrismaClient } from ".prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
-
-const connectionString = process.env.DATABASE_URL!;
-const adapter = new PrismaNeon(connectionString);
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as {
+  prisma?: PrismaClient;
+};
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
+    log: ["query", "error", "warn"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
