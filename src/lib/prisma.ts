f9ce4651b0420850519c796a3a0fa6710e3a2b89
Kapilansh Patil <kapilansh.patil.etrx@ghrce.raisoni.net>
2025-11-26 18:31:16 +0530

    chore: update Prisma dependencies and configuration - Upgraded @prisma/client and prisma to version 7.0.0. - Replaced @prisma/client with @prisma/adapter-neon for Neon database support. - Changed Prisma client generator provider from "prisma-client" to "prisma-client-js". - Removed the engine configuration from prisma.config.ts. - Updated prisma.ts to use PrismaNeon adapter for database connection.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -13,13 +13,18 @@
 
 
 import { PrismaClient } from "../generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaNeon(connectionString);
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query", "error", "warn"],
+    adapter,
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
